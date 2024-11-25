670ecb2c2a51b29a21aa15934cfa38da11f010e9
Milwad <milwad.dev@gmail.com>
2024-11-25 21:56:25 +0330

    wip

--- tests/Feature/AddScoreTest.php
+++ tests/Feature/AddScoreTest.php
@@ -1,4 +1,32 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\SetUp\Models\Photo;
+use Tests\SetUp\Models\User;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+
+/*
+ * Use `RefreshDatabase` for delete migration data for each tests.
+ */
+uses(RefreshDatabase::class);
+
 test('user can give score to scoreable', function () {
+    $user = User::query()->create([
+        'name' => 'milwad',
+        'email' => 'milwad@gmail.com',
+        'password' => bcrypt('password'),
+    ]);
+    $photo = Photo::query()->create([
+        'name' => fake()->name,
+    ]);
+
+    $user->addScore($photo);
+
+    // DB Assertions
+    assertDatabaseCount('scores', 1);
+    assertDatabaseHas('scores', [
+        'score' => 1,
+        'scoreable_id' => $photo->getKey()
+    ]);
 });
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,11 @@
 namespace Tests;
 
 use Binafy\LaravelScore\Providers\LaravelScoreServiceProvider;
-use Illuminate\Encryption\Encrypter;
 use Illuminate\Support\Facades\Artisan;
-use Tests\SetUp\Models\User;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+
     /**
      * Load package service provider.
      *
@@ -28,24 +27,13 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
-        // Set default database to use sqlite :memory:
+        // Setup default database to use sqlite :memory:
         $app['config']->set('database.default', 'testing');
         $app['config']->set('database.connections.testing', [
             'driver'   => 'sqlite',
             'database' => ':memory:',
             'prefix'   => '',
         ]);
-
-        // Set app key
-        $app['config']->set('app.key', 'base64:'.base64_encode(
-            Encrypter::generateKey(config()['app.cipher'])
-        ));
-
-        // Set user model
-        $app['config']->set('auth.providers.users.model', User::class);
-
-        // Set user model for monitoring config
-        $app['config']->set('user-monitoring.user.model', User::class);
     }
 
     protected function setUp(): void
