9f8b4024c722761eda9f0d2b73c9ef6d3b2d1367
Milwad <milwad.dev@gmail.com>
2024-11-26 19:28:19 +0330

    add publish config and migration

--- src/Providers/LaravelScoreServiceProvider.php
+++ src/Providers/LaravelScoreServiceProvider.php
@@ -14,4 +14,20 @@ class LaravelScoreServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations/');
         $this->mergeConfigFrom(__DIR__ . '/../../config/laravel-score.php', 'laravel-score');
     }
+
+    /**
+     * Boot the application's service providers.
+     */
+    public function boot(): void
+    {
+        // Config
+        $this->publishes([
+            __DIR__ . '/../../config/laravel-score.php' => config_path('laravel-score.php'),
+        ], 'laravel-score-config');
+
+        // Migrations
+        $this->publishes([
+            __DIR__ . '/../../database/migrations/' => database_path('migrations')
+        ], 'laravel-score-migrations');
+    }
 }
